f1f37b33b1a7b60bfb3541fdb26096dce6aa3f75
jstockfisch <jan@jan-stockfisch.de>
2019-11-19 19:45:28 +0100

    [TASK] Add copyright header to all classes

--- Classes/Command/EnvironmentCommandController.php
+++ Classes/Command/EnvironmentCommandController.php
@@ -3,6 +3,18 @@ declare(strict_types = 1);
 
 namespace KamiYang\T3cvExport\Command;
 
+/*
+ * This file is part of the TYPO3 CONF VARS Export project.
+ *
+ * It is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * For the full copyright and license information, please read
+ * LICENSE file that was distributed with this source code.
+ */
+
 use KamiYang\T3cvExport\Service\DotEnvFileWriterServiceInterface;
 use KamiYang\T3cvExport\Service\EnvironmentServiceInterface;
 use TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
